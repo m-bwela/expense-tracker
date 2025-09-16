3ef7d0a905bd4afbb02c66be22ee476d01a61c02
TyeMuzan <TyeMuzan@gmail.com>
2025-09-16 17:30:25 +0300

    updated HEAD

--- client side/src/components/Heading.jsx
+++ client side/src/components/Heading.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Heading() {
-    const date = new Date();
     return (
         <>
             <h1 className="heading">Personal Expense Tracker.</h1>
