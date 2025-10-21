089045d9bfde47065bdf2fe769aefaeeb5760ff4
TyeMuzan <TyeMuzan@gmail.com>
2025-10-21 16:48:12 +0300

    Added database configuration to .env file

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import pg from 'pg';
+import env from 'dotenv';
+
+env.config();
+
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+db.connect();
+
+module.exports = db;
