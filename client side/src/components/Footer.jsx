e01019133ac2fa0f79807767f99ee9f5622280ab
TyeMuzan <TyeMuzan@gmail.com>
2025-12-20 17:29:04 +0300

    Updated footer component styling and links

--- client side/src/components/Footer.jsx
+++ client side/src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <p>&copy; {year} Personal Expense Tracker. All rights reserved.</p>
             <p>Created by {yourName}</p>
             <p>Follow me on:</p>
-            <a className="fa fa-linkedin" href="https://ke.linkedin.com/in/tye-joseph-0bb158239/"></a>
+            <a className="fa fa-linkedin" href="https://www.linkedin.com/in/tye-nzambu-07254muzan"></a>
             <a className="fa fa-twitter" href="https://x.com/Tyejoseph1"></a>
             <a className="fa fa-github" href="https://github.com/m-bwela"></a>
         </footer>
